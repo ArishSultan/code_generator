654a6954ee7e5b906ab09161501aaaefa0add270
Arish Khan <arishsultan104@gmail.com>
2021-08-31 22:12:21 +0500

    * added `path` dependency. * changed [CodeWriter] API to support os specific path joining.

--- lib/src/io/writer.dart
+++ lib/src/io/writer.dart
@@ -1,8 +1,24 @@
 import 'dart:io';
 
+import 'package:path/path.dart';
+
 class CodeWriter {
-  static Future<void> prepareDir(String path) async {
-    final directory = Directory(path);
+  static Future<void> write(List<String> pathSegments, String content) async {
+    final file = File(joinAll(pathSegments));
+
+    if (!await file.exists()) await file.create();
+    await file.writeAsString(content);
+  }
+
+  static void writeSync(List<String> pathSegments, String content) {
+    final file = File(joinAll(pathSegments));
+
+    if (!file.existsSync()) file.createSync();
+    file.writeAsStringSync(content);
+  }
+
+  static Future<void> prepareDir(List<String> pathSegments) async {
+    final directory = Directory(joinAll(pathSegments));
     if (await directory.exists()) {
       await directory.delete(recursive: true);
     }
@@ -10,8 +26,8 @@ class CodeWriter {
     await directory.create();
   }
 
-  static void prepareDirSync(String path) {
-    final directory = Directory(path);
+  static void prepareDirSync(List<String> pathSegments) {
+    final directory = Directory(joinAll(pathSegments));
     if (directory.existsSync()) {
       directory.deleteSync(recursive: true);
     }
