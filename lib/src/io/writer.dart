12de86e5473b6de3779f3e74f6c0e0cfa1d67617
Arish Khan <arishsultan104@gmail.com>
2021-09-03 10:00:00 +0500

    added [CodeFormatter] support in [CodeWriter]

--- lib/src/io/writer.dart
+++ lib/src/io/writer.dart
@@ -2,22 +2,27 @@ import 'dart:io';
 
 import 'package:path/path.dart';
 
+typedef CodeFormatter = String Function(String);
+
 class CodeWriter {
-  static Future<void> write(List<String> pathSegments, String content) async {
+  final _formatters = <String, CodeFormatter>{};
+
+  void registerFormatter(String name, CodeFormatter formatter) =>
+      _formatters[name] = formatter;
+
+  void unregisterFormatters(String name) => _formatters.remove(name);
+
+  Future<void> write(List<String> pathSegments, String content) async {
     final file = File(joinAll(pathSegments));
 
     if (!await file.exists()) await file.create();
-    await file.writeAsString(content);
+
+    await file.writeAsString(
+      _formatters.values.fold(content, (value, e) => e(value.toString())),
+    );
   }
 
-  static void writeSync(List<String> pathSegments, String content) {
-    final file = File(joinAll(pathSegments));
-
-    if (!file.existsSync()) file.createSync();
-    file.writeAsStringSync(content);
-  }
-
-  static Future<void> prepareDir(List<String> pathSegments) async {
+  Future<void> prepareDir(List<String> pathSegments) async {
     final directory = Directory(joinAll(pathSegments));
     if (await directory.exists()) {
       await directory.delete(recursive: true);
@@ -26,7 +31,14 @@ class CodeWriter {
     await directory.create();
   }
 
-  static void prepareDirSync(List<String> pathSegments) {
+  void writeSync(List<String> pathSegments, String content) {
+    final file = File(joinAll(pathSegments));
+
+    if (!file.existsSync()) file.createSync();
+    file.writeAsStringSync(content);
+  }
+
+  void prepareDirSync(List<String> pathSegments) {
     final directory = Directory(joinAll(pathSegments));
     if (directory.existsSync()) {
       directory.deleteSync(recursive: true);
